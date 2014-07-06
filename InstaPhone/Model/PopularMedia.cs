48efe7edf10e63e103a04c7ba467b1ab3b320af1
gruver97 <gruver97@gmail.com>
2014-07-06 20:43:58 +0400

    Добавлен недостающий файл модели.

---
+++ InstaPhone/Model/PopularMedia.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace InstaPhone.Model
+{
+    public class PopularMedia
+    {
+        [JsonProperty("images")]
+        public InstagramImage InstagramImages { get; set; }
+
+        [JsonProperty("likes")]
+        public Likes Likes { get; set; }
+
+        [JsonProperty("type")]
+        public string MediaType { get; set; } 
+    }
+}
